7e9b990363741725700e202a62431fa1c219f70d
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2021-12-29 20:54:11 +0530

    changed theme colors.

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -12,9 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: const RandomWords(),
     );
   }
   // endregion
