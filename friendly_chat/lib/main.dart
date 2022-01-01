914ea24b3e144ab3a610f14922755336df84d700
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-01-01 23:50:51 +0530

    made states

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -26,20 +26,25 @@ class ChatScreen extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  @override 
+  State<ChatScreen> createState() => _ChatScreenState();
+
+}
+  class _ChatScreenState extends State<ChatScreen> {
+    final _textController = TextEditingController();
+
+    void _handleSubmitted(String text) {
+      _textController.clear();
+    }
+    @override
+    Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('FriendlyChat'),
-      ),
+      appBar: AppBar(title: const Text('FriendlyChat')),
+      body: _buildTextComposer(),
     );
   }
+  
+
+  }
+  
 }
