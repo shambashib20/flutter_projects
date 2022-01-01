8c09576527b5bea32e0fe0a33cfe300e773c7a3b
Shambashib Majumdar <shambashibmajumdar2000@gmail.com>
2022-01-02 00:06:50 +0530

    completed stage 4

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -21,30 +21,54 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     Key? key,
   }) : super(key: key);
 
-  @override 
+  @override
   State<ChatScreen> createState() => _ChatScreenState();
-
 }
-  class _ChatScreenState extends State<ChatScreen> {
-    final _textController = TextEditingController();
 
-    void _handleSubmitted(String text) {
-      _textController.clear();
-    }
-    @override
-    Widget build(BuildContext context) {
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('FriendlyChat')),
       body: _buildTextComposer(),
     );
   }
-  
 
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  
 }
